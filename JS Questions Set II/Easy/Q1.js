7e1c12a733db07e881acde8a48262cc046593eb1
WayneMilesIII <milesway42@gmail.com>
2026-02-03 09:03:01 -0800

    Add convertToFahrenheit function to convert Celsius to Fahrenheit

--- JS Questions Set II/Easy/Q1.js
+++ JS Questions Set II/Easy/Q1.js
@@ -1,6 +1,9 @@
 function convertToFahrenheit(celsius) {
-    return (celsius * 9/5) + 32;
+    return console.log((celsius * 9/5) + 32);
 }
 
-convertToFahrenheit(0); // 32
+convertToFahrenheit(-40); // 32
+convertToFahrenheit(0); // 212
 convertToFahrenheit(100); // 212
+convertToFahrenheit(37); // 98.6
+convertToFahrenheit(-19); // 77 
